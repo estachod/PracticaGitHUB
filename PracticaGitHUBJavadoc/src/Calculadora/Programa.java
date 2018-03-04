9cc3b2ae297c3357ebc919b2603ead7d2f43e864
victor <victor@MintEntornos>
2018-03-04 22:26:37 +0100

    Añadidas las funciones

--- PracticaGitHUBJavadoc/src/Calculadora/Programa.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -5,7 +5,21 @@ public class Programa {
 	//TODO: Crear un programa principal que 
 	//utilice todas las funciones de la calculadora
 	//Incluida las operaciones toString y la potencia
-	//que habréis añadido.
+	//que habrï¿½is aï¿½adido.
 	//Ejecutadlo.
-
+	
+	
+	
+	
+	static Calculadora calc = new Calculadora(3,4);
+	
+	
+	public static void main(String[] args) {
+		System.out.println(calc.toString());
+		System.out.println("Suma = " + calc.suma());
+		System.out.println("Resta = " + calc.resta());
+		System.out.println("Multiplicacion = " + calc.potencia());
+		System.out.println("Division = " + calc.division());
+	}
+	
 }
