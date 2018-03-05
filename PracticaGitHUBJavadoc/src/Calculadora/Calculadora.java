bff22ed8611a9ebeb7ae9c64e21c9913521a9bd2
victor <victor@MintEntornos>
2018-03-05 01:56:04 +0100

    Añadido javadoc

--- PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Calculadora.java
@@ -2,24 +2,53 @@ package Calculadora;
 
 public class Calculadora {
 
-	double op1;
-	double op2;
+	/*
+	 * Es un atrubuto de tipo double, operador 1
+	 * @param op1 double numero real
+	 */
+	
+	public double op1;
+	/*
+	 * Es un atributo de tipo double, operador 2
+	 * @param op2 double numero real
+	 */
+	
+	public double op2;
+	
+	/*
+	 * Es un constructor que permite generar objetos de la clase calculadora, llama a los metodos
+	 * @param op1 double numero real
+	 * @param op2 double numero real
+	 */
 	
 	
 	public Calculadora(double op1, double op2) {
 		this.op1 = op1;
 		this.op2 = op2;
 	}
+	/*
+	 * El atibuto suma recoge el return
+	 * @return devuelve el resultado de sumar el op1 + op2
+	 */
 	
-	double suma() {
+	public double suma() {
 		return op1+op2;
 	}
+	/*
+	 * el atributo resta recoge el return
+	 * @return devuelve el resultado de restar op1 - op2
+	 */
 	
-	double resta() {
+	public double resta() {
 		return op1-op2;
 	}
+	/*
+	 * el atributo divison recoge el return
+	 * @return devuelve el resultado de dividir op1/op2
+	 * @throws IllegalArgumentException evita que op2 sea 0
+	 */
 	
-	double division() {
+	public double division() {
 		if(op2==0) {
 			throw new IllegalArgumentException("El divisor es 0");
 		}
@@ -32,8 +61,11 @@ public class Calculadora {
 	//-Un n�mero "n" elevado a una potencia negativa "-p" es igual a 1/(n^p)
 	//As� 232^0 = 1; y 2^(-3)=1/2^3=1/8
 	
-	
-	double potencia() {
+	/*
+	 * el atributo potencia recoge el return
+	 * @return devuelve el resultado de elevar op1 por op2
+	 */
+	 public double potencia() {
 		return Math.pow(op1, op2);
 	}
 	
@@ -43,6 +75,7 @@ public class Calculadora {
 	//"operando1 = <op1>"
 	//"operando2 = <op2>"
 	
+	@Override
 	
 	public String toString() {
 		return "Calculadora de Victor\n"+ "operando1 = "+op1+" operando2 = "+op2;   
